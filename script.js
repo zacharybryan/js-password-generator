296b280d2bd34799cb0709daf0219b13c7d6f938
Zach Bryan <zacharybryan@icloud.com>
2020-07-20 18:51:04 -0700

    added length prompt

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+const length = parseInt(prompt("How long would you like your password? 8 - 128?"));
