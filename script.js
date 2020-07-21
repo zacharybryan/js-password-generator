703d8c519da0ab922fadd130a5f2db5a50d9be55
Zach Bryan <zacharybryan@icloud.com>
2020-07-20 19:03:29 -0700

    added characters and object

--- script.js
+++ script.js
@@ -10,7 +10,38 @@ function writePassword() {
 
 }
 
+const length = parseInt(prompt("How long would you like your password? 8 - 128?"));
+
+
+// Generate Functions 
+
+// http://www.net-comber.com/charset.html Character Set Guide
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+console.log(getRandomLower()); /*works gets random lowercase */
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log(getRandomUpper()); /*works gets random uppercase */
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+console.log(getRandomNumber()); /*works gets random number */
+
+function getRandomSymbol() {
+    const symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomSymbol()); /* works get random symbol */
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-const length = parseInt(prompt("How long would you like your password? 8 - 128?"));
